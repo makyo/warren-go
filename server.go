f5014311400aa0b15d00203064a856ea5f41002f
Madison Scott-Clary <matthew.scott@canonical.com>
2015-03-27 17:29:03 -0600

    Godoc test

--- server.go
+++ server.go
@@ -1,7 +1,8 @@
+// Warren is a networked content-sharing site.
+//
 // Copyright 2015 The Warren Authors
 // Use of this source code is governed by an MIT license that can be found in
 // the LICENSE file.
-
 package main
 
 import (
